60774fe12e5bd10445e4ea7770bd9621ed5cb641
qiquan1011 <1477742998@qq.com>
2021-11-04 16:58:20 +0800

    稳定性测试完整版

--- get_cpu_merry.py
+++ get_cpu_merry.py
@@ -18,7 +18,6 @@ def getProcess(name):
 
         if str(p.name()) == name:
             #start_time=datetime.datetime.now()
-            while True:
                 t = time.localtime()
                 cm_time = '%d:%d:%d' % (t.tm_hour, t.tm_min, t.tm_sec)
                 memory=p.memory_percent()
@@ -27,35 +26,23 @@ def getProcess(name):
                 cpu=p.cpu_percent(None)
                 a=cm_time,memory,cpu
                 num_cls.append(a)
-    return num_cls
+                return cm_time,memory,cpu
+
+def main():
+    now = time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+    fname=now + r"-report.csv"
+    with open("%s" % fname,"w") as f:
+        title_str= "Time,Men,Cpu"
+        f.write(title_str + "\n")
+        while True:
+            info=getProcess("Galaxy.Gemini.Shell.exe")
+            tmp_str= "%4s,%5s,%4s" % (info[0],info[1],info[2])
+            f.write(tmp_str + "\n")
 
 
-
-
-
-
-def get_cpu_and_memory():
-    '''
-将得到的内存使用率和CPU率使用率，写入表格，生成折线图
-:return:
-'''
-excelpath =('D:\\user.xls')  #新建excel文件
-workbook = xlwt.Workbook(encoding='utf-8')  #写入excel文件
-sheet = workbook.add_sheet('Sheet1',cell_overwrite_ok=True)  #新增一个sheet工作表
-headlist=[u'时间',u'内存使用率',u'cpu使用率']   #写入数据头
-row=0
-col=0
-for head in headlist:
-    sheet.write(row,col,head)
-    col=col+1
-num=getProcess("Galaxy.Gemini.Shell.exe")
-for i in range(1,len(num)):
-    for j in range(0,3):
-        sheet.write(i,j,num[i-1][j])
-workbook.save(excelpath) #保存
 
 
 
 if __name__=="__main__":
-        getProcess("Galaxy.Gemini.Shell.exe")
+        main()
 
--- open_close_dynamic.py
+++ open_close_dynamic.py
@@ -1,4 +1,5 @@
 import datetime
+import os.path
 import time
 import psutil
 from pywinauto import  Application
@@ -15,15 +16,17 @@ def openAndclose_holters_and_Abp():
     ggh_handle.wait(wait_for="exists enabled ",timeout=3,retry_interval=3)
     starttime = datetime.datetime.now()
     num=0
+    screen_path="D:\\pythonProject\\pythonProject\\test_Stability\\testScreen\\"
+    if os.path.exists(screen_path) is False:
+        os.mkdir(screen_path)
     while True:
         num+=1
-        path="D:\\pythonProject\\pythonProject\\test_Stability\\testScreen\\"
         try:
             DiagHolterEcg=ggh_handle.child_window(title="动态心电", auto_id="lblItemName", control_type="Text")
             #DiagHolterEcg.wait(wait_for="exists enabled ",timeout=3,retry_interval=3)
             DiagHolterEcg.click_input()
         except:
-            save_path=path+ "第"+ str(num)+"次点击动态心电tab页.png"
+            save_path=screen_path+ "第"+ str(num)+"次点击动态心电tab页.png"
             ggh_handle.capture_as_image().save(save_path)
         #ggh_handle.print_control_identifiers()
         try:
@@ -31,7 +34,7 @@ def openAndclose_holters_and_Abp():
             #a.wait(wait_for="exists enabled ",timeout=3,retry_interval=3)
             a.double_click_input(button='left')
         except:
-            report_path=path+"第"+ str(num)+"次点动列表报告.png"
+            report_path=screen_path+"第"+ str(num)+"次点动列表报告.png"
             ggh_handle.capture_as_image().save(report_path)
         time.sleep(2)
         pids=psutil.pids()
@@ -47,7 +50,7 @@ def openAndclose_holters_and_Abp():
                     handel = you_diag_name_app.window(title="诊断程序", control_type="Window")
                     handel.wait(wait_for="exists enabled ",timeout=3,retry_interval=3)
                 except:
-                    holtel_path=path+"第"+ str(num)+"次连接不上动态心电程序.png"
+                    holtel_path=screen_path+"第"+ str(num)+"次连接不上动态心电程序.png"
                     handel.capture_as_image().save(holtel_path)
             #handel.print_control_identifiers()
                 try:
@@ -55,10 +58,10 @@ def openAndclose_holters_and_Abp():
                     c.wait(wait_for="exists enabled ",timeout=3,retry_interval=3)
                     c.click()
                 except:
-                    holtel_close_path=path+"第"+ str(num)+"次关闭动态心电失败.png"
+                    holtel_close_path=screen_path+"第"+ str(num)+"次关闭动态心电失败.png"
                     handel.capture_as_image().save(holtel_close_path)
         if you_diag_name_app is None:
-            fail_path=path+"第"+ str(num)+"次打开报告失败.png"
+            fail_path=screen_path+"第"+ str(num)+"次打开报告失败.png"
             ggh_handle.capture_as_image().save(fail_path)
 
         try:
@@ -66,14 +69,14 @@ def openAndclose_holters_and_Abp():
             #DiagnoseSelectorControl.wait(wait_for="exists enabled ",timeout=3,retry_interval=3)
             DiagnoseSelectorControl.click_input()
         except:
-            abp_tab_path=path+"第"+ str(num)+"次点击动态血压tab页.png"
+            abp_tab_path=screen_path+"第"+ str(num)+"次点击动态血压tab页.png"
             ggh_handle.capture_as_image().save(abp_tab_path)
         try:
             b=ggh_handle.child_window(title="动态血压", auto_id="DiagAbp", control_type="Pane").child_window(title="行 1", control_type="Custom")
         #b.wait(wait_for="exists enabled ",timeout=3,retry_interval=3)
             b.double_click_input(button='left')
         except:
-            abp_report_path=path+"第"+ str(num)+"次点击血压列表数据.png"
+            abp_report_path=screen_path+"第"+ str(num)+"次点击血压列表数据.png"
             ggh_handle.capture_as_image().save(abp_report_path)
         time.sleep(1)
         pids=psutil.pids()
@@ -88,7 +91,7 @@ def openAndclose_holters_and_Abp():
                     handel_1 = you_abp_name_app.window(title="心电及电生理网络系统软件", control_type="Window")
                     handel_1.wait(wait_for="exists enabled ",timeout=3,retry_interval=3)
                 except:
-                    abp_path=path+"第"+ str(num)+"次连接不上血压程序.png"
+                    abp_path=screen_path+"第"+ str(num)+"次连接不上血压程序.png"
                     handel_1.capture_as_image().save(abp_path)
             #handel_1.print_control_identifiers()
                 try:
@@ -96,10 +99,10 @@ def openAndclose_holters_and_Abp():
                     d.wait(wait_for="exists enabled ",timeout=3,retry_interval=3)
                     d.click_input(button="left")
                 except:
-                    return_path=path+"第"+ str(num)+"次关闭血压程序失败.png"
+                    return_path=screen_path+"第"+ str(num)+"次关闭血压程序失败.png"
                     handel_1.capture_as_image().save(return_path)
         if you_abp_name_app is None:
-                abp_file_path=path+"第"+ str(num)+"次血压报告打开失败.png"
+                abp_file_path=screen_path+"第"+ str(num)+"次血压报告打开失败.png"
                 ggh_handle.capture_as_image().save(abp_file_path)
 
 
